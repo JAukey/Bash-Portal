bb5a8a880504e91335be57d3133d65ac38d584bf
JAukey <jackaukey@126.com>
2016-08-04 15:22:18 +0800

    add port drop

--- portal.sh
+++ portal.sh
@@ -4,31 +4,48 @@ iptables_init(){
 	iptables -t nat -N ndsOUT -w
 	iptables -t mangle -N ndsOUT -w
 	iptables -t mangle -N ndsINC -w
+    iptables -t filter -N ndsNET -w
+    iptables -t filter -N ndsAUT -w
 	for i in ${arr[@]}
 	do
 		iptables -t nat -I PREROUTING 1 -i $i -j ndsOUT -w
 		iptables -t mangle -I PREROUTING 1 -i $i -j ndsOUT -w
 		iptables -t mangle -I POSTROUTING 1 -o $i -j ndsINC -w
+        iptables -t filter -I FORWARD 1 -i $i -j ndsNET -w
 	done
 	iptables -t nat -A ndsOUT -m mark --mark 0x400/0x700 -j ACCEPT -w
+    iptables -t nat -A ndsOUT -p tcp -m tcp --dport 53 -j ACCEPT -w
+    iptables -t nat -A ndsOUT -p udp -m udp --dport 53 -j ACCEPT -w
 	iptables -t nat -A ndsOUT -p tcp -m tcp --dport 80 -j DNAT --to-destination $4 -w	
-	#iptables -t nat -A ndsOUT -p udp  -m udp --dport 53 -j DNAT --to-destination $5 -w	
+	iptables -t nat -A ndsOUT -j ACCEPT -w
+    iptables -t filter -A ndsNET -m state --state INVALID -j DROP -w
+    iptables -t filter -A ndsNET -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu -w
+    iptables -t filter -A ndsNET -m mark --mark 0x400/0x700 -j ndsAUT -w
+    iptables -t filter -A ndsNET -p tcp -m tcp --dport 53 -j ACCEPT -w
+    iptables -t filter -A ndsNET -p udp -m udp --dport 53 -j ACCEPT -w
+    iptables -t filter -A ndsNET -j REJECT --reject-with icmp-port-unreachable -w
+    iptables -t filter -A ndsAUT -m state --state RELATED,ESTABLISHED -j ACCEPT -w
+    iptables -t filter -A ndsAUT -j ACCEPT -w
 }
 iptables_delete(){
 	arr=$1
 	iptables -t nat -F ndsOUT -w
-        iptables -t mangle -F ndsOUT -w
-        iptables -t mangle -F ndsINC -w
+    iptables -t mangle -F ndsOUT -w
+    iptables -t mangle -F ndsINC -w
+    iptables -t filter -F ndsAUT -w
+    iptables -t filter -F ndsNET -w
 	for i in ${arr[@]}                                                               
-        do                                                                                 
-                iptables -t nat -D PREROUTING -i $i -j ndsOUT -w                         
-                iptables -t mangle -D PREROUTING -i $i -j ndsOUT -w                      
-                iptables -t mangle -D POSTROUTING -o $i -j ndsINC -w                     
-        done 
+    do                                                                                 
+        iptables -t nat -D PREROUTING -i $i -j ndsOUT -w                         
+        iptables -t mangle -D PREROUTING -i $i -j ndsOUT -w                      
+        iptables -t mangle -D POSTROUTING -o $i -j ndsINC -w  
+        iptables -t filter -D FORWARD -i $i -j ndsNET -w
+    done 
 	iptables -t nat -X ndsOUT -w
-        iptables -t mangle -X ndsOUT -w
-        iptables -t mangle -X ndsINC -w
-  
+    iptables -t mangle -X ndsOUT -w
+    iptables -t mangle -X ndsINC -w
+    iptables -t filter -X ndsAUT -w
+    iptables -t filter -X ndsNET -w
 }
 client_add(){
 	arr=$1
