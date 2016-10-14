c3db5aef9a4f0506346a89c9a9edef0e1e99b8bb
JAukey <jackaukey@126.com>
2016-10-14 12:35:20 +0800

    new

--- bash_portal.sh
+++ bash_portal.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 VERSION="0.1"
+IPT="iptables -t"
 valid_ip()
 {
     local  ip=$1
@@ -77,31 +78,31 @@ Server ip: $4
     IFS=","                                                                                                                   
     local arr=($1)                                                                                                                  
     IFS="$OLD_IFS"
-    iptables -t nat -N ndsOUT -w
-    iptables -t mangle -N ndsOUT -w
-    iptables -t mangle -N ndsINC -w
-    iptables -t filter -N ndsNET -w
-    iptables -t filter -N ndsAUT -w
+    $IPT nat -N bspOUT -w
+    $IPT mangle -N bspOUT -w
+    $IPT mangle -N bspINC -w
+    $IPT filter -N bspNET -w
+    $IPT filter -N bspAUT -w
     for i in ${arr[@]}
     do
-	iptables -t nat -I PREROUTING 1 -i $i -j ndsOUT -w
-	iptables -t mangle -I PREROUTING 1 -i $i -j ndsOUT -w
-	iptables -t mangle -I POSTROUTING 1 -o $i -j ndsINC -w
-    iptables -t filter -I FORWARD 1 -i $i -j ndsNET -w
+	$IPT nat -I PREROUTING 1 -i $i -j bspOUT -w
+	$IPT mangle -I PREROUTING 1 -i $i -j bspOUT -w
+	$IPT mangle -I POSTROUTING 1 -o $i -j bspINC -w
+    $IPT filter -I FORWARD 1 -i $i -j bspNET -w
      done
-    iptables -t nat -A ndsOUT -m mark --mark 0x400/0x700 -j ACCEPT -w
-    iptables -t nat -A ndsOUT -p tcp -m tcp --dport 53 -j ACCEPT -w
-    iptables -t nat -A ndsOUT -p udp -m udp --dport 53 -j ACCEPT -w
-    iptables -t nat -A ndsOUT -p tcp -m tcp --dport 80 -j DNAT --to-destination $4 -w	
-    iptables -t nat -A ndsOUT -j ACCEPT -w
-    iptables -t filter -A ndsNET -m state --state INVALID -j DROP -w
-    iptables -t filter -A ndsNET -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu -w
-    iptables -t filter -A ndsNET -m mark --mark 0x400/0x700 -j ndsAUT -w
-    iptables -t filter -A ndsNET -p tcp -m tcp --dport 53 -j ACCEPT -w
-    iptables -t filter -A ndsNET -p udp -m udp --dport 53 -j ACCEPT -w
-    iptables -t filter -A ndsNET -j REJECT --reject-with icmp-port-unreachable -w
-    iptables -t filter -A ndsAUT -m state --state RELATED,ESTABLISHED -j ACCEPT -w
-    iptables -t filter -A ndsAUT -j ACCEPT -w
+    $IPT nat -A bspOUT -m mark --mark 0x400/0x700 -j ACCEPT -w
+    $IPT nat -A bspOUT -p tcp -m tcp --dport 53 -j ACCEPT -w
+    $IPT nat -A bspOUT -p udp -m udp --dport 53 -j ACCEPT -w
+    $IPT nat -A bspOUT -p tcp -m tcp --dport 80 -j DNAT --to-destination $4 -w	
+    $IPT nat -A bspOUT -j ACCEPT -w
+    $IPT filter -A bspNET -m state --state INVALID -j DROP -w
+    $IPT filter -A bspNET -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu -w
+    $IPT filter -A bspNET -m mark --mark 0x400/0x700 -j bspAUT -w
+    $IPT filter -A bspNET -p tcp -m tcp --dport 53 -j ACCEPT -w
+    $IPT filter -A bspNET -p udp -m udp --dport 53 -j ACCEPT -w
+    $IPT filter -A bspNET -j REJECT --reject-with icmp-port-unreachable -w
+    $IPT filter -A bspAUT -m state --state RELATED,ESTABLISHED -j ACCEPT -w
+    $IPT filter -A bspAUT -j ACCEPT -w
 }
 iptables_delete(){
     valid_interface $1
@@ -115,23 +116,23 @@ iptables_delete(){
     else
         rm -f /tmp/portal.list
     fi
-    iptables -t nat -F ndsOUT -w
-    iptables -t mangle -F ndsOUT -w
-    iptables -t mangle -F ndsINC -w 
-    iptables -t filter -F ndsAUT -w
-    iptables -t filter -F ndsNET -w
+    $IPT nat -F bspOUT -w
+    $IPT mangle -F bspOUT -w
+    $IPT mangle -F bspINC -w 
+    $IPT filter -F bspAUT -w
+    $IPT filter -F bspNET -w
     for i in ${arr[@]}                                                               
     do                                                                                 
-        iptables -t nat -D PREROUTING -i $i -j ndsOUT -w                         
-        iptables -t mangle -D PREROUTING -i $i -j ndsOUT -w                      
-        iptables -t mangle -D POSTROUTING -o $i -j ndsINC -w  
-        iptables -t filter -D FORWARD -i $i -j ndsNET -w
+        $IPT nat -D PREROUTING -i $i -j bspOUT -w                         
+        $IPT mangle -D PREROUTING -i $i -j bspOUT -w                      
+        $IPT mangle -D POSTROUTING -o $i -j bspINC -w  
+        $IPT filter -D FORWARD -i $i -j bspNET -w
     done 
-    iptables -t nat -X ndsOUT -w
-    iptables -t mangle -X ndsOUT -w
-    iptables -t mangle -X ndsINC -w
-    iptables -t filter -X ndsAUT -w
-    iptables -t filter -X ndsNET -w
+    $IPT nat -X bspOUT -w
+    $IPT mangle -X bspOUT -w
+    $IPT mangle -X bspINC -w
+    $IPT filter -X bspAUT -w
+    $IPT filter -X bspNET -w
 }
 iptables_status(){
 local clientcount=-1
@@ -147,8 +148,8 @@ do
     else
         if [ "$line" = "++++++++++++++++++++" ];then
             clientcount=0
-            echo "Total download: $(iptables -n -v -t mangle -L POSTROUTING | awk 'NR==3 {print $2}')"	
-            echo "Total upload: $(iptables -n -v -t mangle -L PREROUTING | awk 'NR==3 {print $2}')"	
+            echo "Total download: $($IPT -n -v -t mangle -L POSTROUTING -w | awk 'NR==3 {print $2}')"	
+            echo "Total upload: $($IPT -n -v -t mangle -L PREROUTING -w | awk 'NR==3 {print $2}')"	
             echo "++++++++++++++++++++"
         else
             if [ $clientcount -ge 0 ];then
@@ -161,8 +162,8 @@ do
                 echo "IP: ${arr[2]} MAC: ${arr[1]}"
                 echo "Added time: $(date -d @${arr[0]})"
                 echo "Added duration: "$[$rangetime/86400]" Day "$[$rangetime%86400/3600]" Hour "$[$rangetime%3600/60]" Min "$[$rangetime%60]" Sec"
-                echo "Download: $(iptables -n -v -t mangle -L ndsINC | grep -w "${arr[2]}" | awk 'NR==1 {print $2}')"
-                echo "Upload: $(iptables -n -v -t mangle -L ndsOUT | grep -w "${arr[2]}" | awk 'NR==1 {print $2}')"
+                echo "Download: $($IPT -n -v -t mangle -L bspINC -w | grep -w "${arr[2]}" | awk 'NR==1 {print $2}')"
+                echo "Upload: $($IPT -n -v -t mangle -L bspOUT -w | grep -w "${arr[2]}" | awk 'NR==1 {print $2}')"
             else
             echo $line
             fi
@@ -182,16 +183,16 @@ client_add(){
 	for i in ${arr[@]}
 	do
 		valid_ip $i
-		if [ -n "$(iptables-save | grep -w "ndsOUT -s $i/32")" ];then        
+		if [ -n "$(iptables-save | grep -w "bspOUT -s $i/32")" ];then        
     		        echo "IP is in the iptables!"
 			continue	
 		fi 
 		arpmac=$(cat /proc/net/arp | grep -w "$i" | awk '{print $4}')
 		if [ -n "$arpmac" ];then
 	        sed -i '$a '$(date +%s)' '$arpmac' '$i'' /tmp/portal.list
-			iptables -t mangle -A ndsINC -d $i/32 -j MARK --set-xmark 0xa400/0xa400 -w
-			iptables -t mangle -A ndsINC -d $i/32 -j ACCEPT -w
-			iptables -t mangle -A ndsOUT -s $i/32 -m mac --mac-source $arpmac -j MARK --set-xmark 0xa400/0xa400 -w
+			$IPT mangle -A bspINC -d $i/32 -j MARK --set-xmark 0xa400/0xa400 -w
+			$IPT mangle -A bspINC -d $i/32 -j ACCEPT -w
+			$IPT mangle -A bspOUT -s $i/32 -m mac --mac-source $arpmac -j MARK --set-xmark 0xa400/0xa400 -w
 		else
 			echo "ip is not in arp!"
 		fi
@@ -209,16 +210,16 @@ client_delete(){
 	for i in ${arr[@]}
 	do
 		valid_ip $i
-		if [ -z "$(iptables-save | grep -w "ndsOUT -s $i/32")" ];then                                                 
+		if [ -z "$(iptables-save | grep -w "bspOUT -s $i/32")" ];then                                                 
             echo "IP is not in the iptables!"                                                                                      
             continue                                                                                              
         fi 
 		arpmac=$(cat /proc/net/arp | grep -w "$i" | awk '{print $4}')
 		if [ -n "$arpmac" ];then
 			sed -i '/'$arpmac'/d' /tmp/portal.list
-			iptables -t mangle -D ndsINC -d $i/32 -j MARK --set-xmark 0xa400/0xa400 -w
-			iptables -t mangle -D ndsINC -d $i/32 -j ACCEPT -w
-			iptables -t mangle -D ndsOUT -s $i/32 -m mac --mac-source $arpmac -j MARK --set-xmark 0xa400/0xa400 -w
+			$IPT mangle -D bspINC -d $i/32 -j MARK --set-xmark 0xa400/0xa400 -w
+			$IPT mangle -D bspINC -d $i/32 -j ACCEPT -w
+			$IPT mangle -D bspOUT -s $i/32 -m mac --mac-source $arpmac -j MARK --set-xmark 0xa400/0xa400 -w
 		else
 			echo "ip is not in arp!"
 		fi
